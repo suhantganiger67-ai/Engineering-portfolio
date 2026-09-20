9a9b6f767d509a9809e19b298d06ba3ea7ea1dbf
suhantganiger67-ai <suhantganiger67@gmail.com>
2026-09-20 16:00:41 +0530

    Create Python revison.py.py

---
+++ Python-scripts/Python revision/Python revison.py.py
@@ -0,0 +1,13 @@
+# arithmetic operators
+print(5 + 3)
+print(5 - 3)
+print(5 * 3)
+print(5 / 3)
+print(5 // 3)
+print(5 % 3)  # modulo -> remainder
+print(5 ** 3)
+
+
+# comparison operators
+print(5 > 3)
+print(5 < 3)
